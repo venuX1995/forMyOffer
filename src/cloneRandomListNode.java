324e89f55c6f703adf95b6bf22ec8fb8fc7b578f
venuX1995 <zhouqingxuan1995@163.com>
2019-04-01 18:56:22 +0800

    practise in 04.01

---
+++ src/cloneRandomListNode.java
@@ -0,0 +1,36 @@
+public class cloneRandomListNode {
+    public class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    public RandomListNode Clone(RandomListNode pHead)
+    {
+        if(pHead == null)
+            return null;
+        RandomListNode pCur = pHead;
+        while(pCur!=null){       //在原链表的每个节点后面复制新的节点
+            RandomListNode pClone = new RandomListNode(pCur.label);
+            pClone.next = pCur.next;
+            pCur.next = pClone;
+            pCur = pClone.next;
+        }
+        pCur = pHead;
+        while(pCur!=null){
+            pCur.next.random = pCur.random==null?null:pCur.random.next;
+            pCur = pCur.next.next;
+        }
+        pCur = pHead;
+        RandomListNode pNewHead =pHead.next;
+        while (pCur!=null){
+
+        }
+        return pNewHead;
+    }
+
+}
